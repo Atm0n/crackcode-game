2ba95a073df1533d2f87900e2309ce0d297e4c03
Nil <Atm0n@users.noreply.github.com>
2018-01-28 19:17:51 +0100

    changes
    changes

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import time, random
 difficulty = 2 + int(input("Choose the dificulty, 3 for easy, 2 for normal, 1 for HARD: "))
+difficulty2 =difficulty
 clues = list()
 def generator():
     secret = str(random.randint(0, 999))
@@ -8,26 +9,26 @@ def generator():
     else:
         for a in range(1, 4 - len(secret)):
             secret = "0" + secret
-            # if the random number isn't 3 numbers large it fix them
-    print("DONE")
+            # if the random number isn't 3 numbers large it fix them.
     return secret
 # CLUES
 
 
-while difficulty > 0:
+while difficulty2 > 0:
     clues.append(generator())
-    difficulty = difficulty -1
+    difficulty2 = difficulty2 -1
 
 code = generator()
 
 print(clues)
 print(code + "code")
-for a in range(0, 5):
-    print(a)
-    for b in clues[a]:
-        print(b +"hola")
+for a in range(0, difficulty):
+    for b in list(clues[a]):
         for c in code:
             if b == c:
-                print(str(c) +"==" +str(b))
+                print(a)
+                print(b)
+
+                print(str(c) + "==" + str(b))
 
 
